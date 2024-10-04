c27f7ec77290f98110cf182508677d89f81c60fa
Luukas Pörtfors <luukas.portfors@aalto.fi>
2024-10-04 18:00:23 +0300

    feat: add sbt and x dependencies

--- devenv.nix
+++ devenv.nix
@@ -1,9 +1,18 @@
-{
-  languages.scala.enable = true;
+{pkgs, ...}: {
+  languages.scala = {
+    enable = true;
+    sbt.enable = true;
+  };
+  packages = with pkgs; [
+    xorg.libX11
+    xorg.libXext
+    xorg.libXtst
+  ];
+
   devcontainer = {
     enable = true;
     settings = {
-      options.updateContentCommand = "devenv build";
+      #updateContentCommand = "devenv build";
       customizations.vscode.extensions = [
         "scalameta.metals"
         "scala-lang.scala"
