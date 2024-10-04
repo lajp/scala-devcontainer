de7983010985cb30d128eec268eddcf35f6b8967
Luukas Pörtfors <luukas.portfors@aalto.fi>
2024-10-04 16:19:09 +0300

    init

---
+++ devenv.nix
@@ -0,0 +1,10 @@
+{
+  languages.scala.enable = true;
+  devcontainer = {
+    enable = true;
+    settings.customizations.vscode.extensions = [
+      "scalameta.metals"
+      "scala-lang.scala"
+    ];
+  };
+}
